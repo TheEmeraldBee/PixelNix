80adfe6979265aafe2a733355cf82d19b97e2d88
brightonlcox <brightonlcox@gmail.com>
2024-10-26 22:46:58 -0600

    Added Zellij Layouts To Home Manager

--- home-manager/zellij.nix
+++ home-manager/zellij.nix
@@ -7,4 +7,41 @@
       pane_frames = false;
     };
   };
+
+  xdg.configFile."zellij/layouts/default.kdl".text = ''
+    layout {
+      tab_template name="ui" {
+          children
+      }
+
+      default_tab_template {
+          pane name="sidebar"
+      }
+    }
+  '';
+
+  xdg.configFile."zellij/layouts/default.swap.kdl".text = ''
+    swap_tiled_layout name="one_pane" {
+      ui exact_panes=2 {
+        pane split_direction="vertical" {
+          pane name="sidebar" size="20%"
+          pane name="editor" command="hx"
+        }
+      }
+    }
+
+    swap_tiled_layout name="sidebar_open_two_or_more_panes" {
+      ui min_panes=3 {
+        pane split_direction="vertical" {
+          pane name="sidebar" size="20%"
+          pane split_direction="horizontal" {
+            pane name="editor" command="hx" size="70%"
+
+            pane stacked=true { children; }
+          }
+        }
+      }
+    }
+
+  '';
 }
