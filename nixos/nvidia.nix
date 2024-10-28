fbd811a7443b124b685f02ff87ccf89c631491f9
brightonlcox <brightonlcox@gmail.com>
2024-10-28 16:44:10 -0600

    93 current  2024-10-28 16:44:02  24.11.20241018.4c2fcb0  6.6.57                          *

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -29,11 +29,14 @@
 
     forceFullCompositionPipeline = true;
 
-    # prime = {
-    #   sync.enable = true;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
 
-    #   intelBusId = "PCI:0:2:0";
-    #   nvidiaBusId = "PCI:1:0:0";
-    # };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 }
