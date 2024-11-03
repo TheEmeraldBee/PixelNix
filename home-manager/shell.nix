2d13b102fd8247800aa2c7ff033966a7bdc81d22
brightonlcox <brightonlcox@gmail.com>
2024-11-03 13:12:04 -0700

    95 current  2024-11-01 22:46:58  24.11.20241018.4c2fcb0  6.6.57                          *

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -1,67 +1,4 @@
 {pkgs, ...}: {
-  # programs.zsh = {
-  #   enable = true;
-  #   enableCompletion = true;
-  #   autosuggestion.enable = true;
-  #   syntaxHighlighting.enable = true;
-
-  #   shellAliases = {
-  #     # File Listing
-  #     ls = "eza -l --icons";
-  #     ll = "eza -Alg --icons";
-  #     lf = "eza -lgTL2 --icons";
-  #     lg = "eza -lgTL4 --git-ignore --icons";
-
-  #     # Zoxide
-  #     cd = "z";
-
-  #     # Zellij
-  #     e = "zellij action new-tab --layout ~/.config/zellij/layouts/ide.kdl";
-
-  #     # Rust
-  #     cup = "cargo +nightly -Zunstable-options update --breaking";
-  #     bc = "bacon clippy";
-
-  #     # Dispatching Windows
-  #     open = "hyprctl dispatch exec";
-  #     float = "hyprctl dispatch exec \"[float;]\"";
-  #     perf = "export __NV_PRIME_RENDER_OFFLOAD=1 &&\
-  #               export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0 &&\
-  #               export __GLX_VENDOR_LIBRARY_NAME=nvidia &&\
-  #               export __VK_LAYER_NV_optimus=NVIDIA_only && hyprctl dispatch exec";
-
-  #     # Nix Shell
-  #     nix-shell = "nix-shell --run zsh";
-
-  #     # Feh
-  #     img = "hyprctl dispatch -- exec \"[float; size 1920 1080;]\" feh -.";
-
-  #     # OS Stuff
-  #     ne = "~/dotfiles/scripts/edit.sh";
-  #   };
-
-  #   plugins = [
-  #     {
-  #       name = "zsh-nix-shell";
-  #       file = "nix-shell.plugin.zsh";
-  #       src = pkgs.fetchFromGitHub {
-  #         owner = "chisui";
-  #         repo = "zsh-nix-shell";
-  #         rev = "v0.8.0";
-  #         sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-  #       };
-  #     }
-  #   ];
-  # };
-
-  # programs.zsh.oh-my-zsh = {
-  #   enable = true;
-  #   plugins = [
-  #     "git"
-  #     "sudo"
-  #   ];
-  # };
-
   programs.nushell = {
     enable = true;
     shellAliases = {
@@ -79,14 +16,13 @@
     };
   };
 
-  programs.zoxide = {
+  programs.carapace = {
     enable = true;
-    # enableZshIntegration = true;
     enableNushellIntegration = true;
   };
 
-  # programs.fzf = {
-  #   enable = true;
-  #   enableZshIntegration = true;
-  # };
+  programs.zoxide = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
 }
