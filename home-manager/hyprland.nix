572f767ba62efee5547dfd03c3b7e5bc3e82e62e
brightonlcox <brightonlcox@gmail.com>
2024-11-09 12:58:35 -0700

    101 current  2024-11-09 10:40:39  24.11.20241018.4c2fcb0  6.6.57                          *

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -7,12 +7,6 @@
   services.hypridle = {
     enable = true;
     settings = {
-      general = {
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-        ignore_dbus_inhibit = false;
-        lock_cmd = "hyprlock";
-      };
-
       listener = [
         {
           timeout = 900;
