fdddf85c2d6fc8e56b57325bf827b21d1d0a2f38
brightonlcox <brightonlcox@gmail.com>
2024-11-09 09:21:52 -0700

    99 current  2024-11-09 08:58:41  24.11.20241018.4c2fcb0  6.6.57                          *

--- packages/neovim.nix
+++ packages/neovim.nix
@@ -3,6 +3,25 @@
     enable = true;
 
     colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
+
+    plugins = {
+      lualine.enable = true;
+
+      telescope.enable = true;
+
+      oil.enable = true;
+
+      treesitter.enable = true;
+
+      luasnip.enable = true;
+
+      lsp = {
+        enable = true;
+        servers = {
+          lua-ls.enable = true;
+          rust-analyzer.enable = true;
+        };
+      };
+    };
   };
 }
