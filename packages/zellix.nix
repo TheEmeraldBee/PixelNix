7e94f60f3a14099bc8a76651506574d36fc65115
brightonlcox <brightonlcox@gmail.com>
2024-11-20 12:57:12 -0700

    110 current  2024-11-12 17:20:12  24.11.20241018.4c2fcb0  6.6.57                          * - nixos

--- packages/zellix.nix
+++ packages/zellix.nix
@@ -2,7 +2,7 @@
   xdg.configFile."zellix" = {
     source = builtins.fetchGit {
       url = "https://github.com/TheEmeraldBee/zellix";
-      rev = "8d20ecde0c34821eadce3e1a377e4115a08213a8";
+      rev = "79f8355abce6c37e37ceb9a1ce27f1b3697dd30a";
     };
     recursive = true;
   };
