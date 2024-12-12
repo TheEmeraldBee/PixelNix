64fa7b9112374cc6a39229a40cd222a8b361bc29
brightonlcox <brightonlcox@gmail.com>
2024-12-11 17:10:53 -0700

    114 current  2024-12-11 16:44:08  24.11.20241018.4c2fcb0  6.6.57                          * - nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -31,6 +31,8 @@
     };
   };
 
+  security.polkit.enable = true;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
