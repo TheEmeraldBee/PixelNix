aca012045b18397c406193acd5933fe3a625bb43
brightoncox <brightonlcox@gmail.com>
2025-01-16 11:38:58 -0700

    126   2025-01-16 11:38:35   (current) - macos

--- macos/configuration.nix
+++ macos/configuration.nix
@@ -24,6 +24,7 @@
     taps = [
       "nikitabobko/tap" # aerospace
       "FelixKratz/formulae" # jankyborders
+      "homebrew/services"
     ];
     brews = [
       "FelixKratz/formulae/borders"
