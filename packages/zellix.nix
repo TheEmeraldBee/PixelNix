a6b9d0eefeaf8739652b1c09f11bd3126d5fda89
brightoncox <brightonlcox@gmail.com>
2025-02-27 14:09:17 -0700

    220   2025-02-27 14:08:45   (current) - macos

--- packages/zellix.nix
+++ packages/zellix.nix
@@ -2,7 +2,7 @@
   xdg.configFile."zellix" = {
     source = builtins.fetchGit {
       url = "https://github.com/TheEmeraldBee/zellix";
-      rev = "c7f36f01a7bf4d447ff6a9b011303a1eec1c7be4";
+      rev = "05d07c3bb65bcb3279ffae71125f899f18838ceb";
     };
     recursive = true;
   };
