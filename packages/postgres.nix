7fc1ad862a88dcdb45ab13c1389af0fc91dae4db
brightoncox <brightonlcox@gmail.com>
2025-04-08 14:57:33 -0600

    259   2025-04-08 14:56:39   (current) - macos

--- packages/postgres.nix
+++ packages/postgres.nix
@@ -1,7 +1,6 @@
 {...}: {
   services.postgresql = {
     enable = true;
-    ensureDatabases = ["pluto"];
     authentication = ''
       #type database  DBuser  auth-method
       local all       all     trust
