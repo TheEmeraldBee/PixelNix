9873395fa295361f27161d7477dc0d153a9658a0
brightoncox <brightonlcox@gmail.com>
2025-04-10 12:22:58 -0600

    268   2025-04-10 12:22:48   (current) - macos

--- shared/shell.nix
+++ shared/shell.nix
@@ -34,6 +34,16 @@
       }
 
       $env.EDITOR = "hx"
+
+      def --env y [...args] {
+      	let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+      	yazi ...$args --cwd-file $tmp
+      	let cwd = (open $tmp)
+      	if $cwd != "" and $cwd != $env.PWD {
+      		cd $cwd
+      	}
+      	rm -fp $tmp
+      }
     '';
     shellAliases = {
       ls = "eza -l --icons";
