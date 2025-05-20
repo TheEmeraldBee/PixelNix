85b51691de8968bee41aa9a2591c00699dc10cfa
Brighton Cox <brightonlcox@gmail.com>
2025-05-19 21:27:15 -0600

    Fixed Cursor In Theming

--- packages/wayland/theme.nix
+++ packages/wayland/theme.nix
@@ -1,51 +1,43 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
-    name = "Catppuccin-Mocha-Dark-Cursors";
-    package = pkgs.catppuccin-cursors.mochaDark;
-    size = 16;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 20;
   };
 
   gtk = {
     enable = true;
     theme = {
-      name = "catppuccin-macchiato-green-compact+rimless,black";
+      name = "catppuccin-macchiato-green-compact+rimless";
       package = pkgs.catppuccin-gtk.override {
         accents = ["green"];
         size = "compact";
-        tweaks = ["rimless" "black"];
+        tweaks = ["rimless"];
         variant = "macchiato";
       };
     };
-
-    cursorTheme = {
-      name = "Catppuccin-Mocha-Dark-Cursors";
-      package = pkgs.catppuccin-cursors.mochaDark;
-      size = 16;
-    };
   };
 
-  dconf = {
-    enable = true;
-    settings = {
-      "org/gnome/desktop/interface" = {
-        gtk-theme = lib.mkForce "catppuccin-macchiato-green-compact+rimless,black";
-        color-scheme = "prefer-dark";
-      };
-    };
-  };
+  # dconf = {
+  #   enable = true;
+  #   settings = {
+  #     "org/gnome/desktop/interface" = {
+  #       gtk-theme = lib.mkForce "catppuccin-macchiato-green-compact+rimless";
+  #       color-scheme = "prefer-dark";
+  #       cursor-theme = "catppuccin-mocha-dark-cursors";
+  #       cursor-size = 20;
+  #     };
+  #   };
+  # };
 
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk3"; # Or "gtk2" if you prefer the Qt5/6 GTK2 look
-    style = {
-      package = pkgs.gtk3; # Or pkgs.gtk2 if using that platformTheme
-      name = "gtk3"; # Or "gtk2"
-    };
-  };
+  # qt = {
+  #   enable = true;
+  #   platformTheme.name = "gtk3"; # Or "gtk2" if you prefer the Qt5/6 GTK2 look
+  #   style = {
+  #     package = pkgs.gtk3; # Or pkgs.gtk2 if using that platformTheme
+  #     name = "gtk3"; # Or "gtk2"
+  #   };
+  # };
 }
